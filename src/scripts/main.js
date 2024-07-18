5b120e5a002fa3c7e9766117a72a69a22869491d
Monica <monicut3@gmail.com>
2024-07-18 14:18:35 +0200

    feat: add JS code to WIP and some comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -57,3 +57,35 @@ document.addEventListener('DOMContentLoaded', function() {
     setInitialPosition();
     animate();
 });
+
+// BEGIN JS - WIP
+
+//Selecciona el elemento con el id note, que es la figura dentro del juego donde se realizará la acción de salto.
+document.addEventListener('DOMContentLoaded', () => {
+        const note = document.getElementById('note');
+        let isJumping = false; // Variable controla si el personaje está en medio de un salto para evitar saltos múltiples simultáneos.
+
+//Escucha el evento keydown para detectar cuando se presiona la tecla Space o ArrowUp
+    document.addEventListener('keydown', event => {
+        if ((event.code === 'Space' || event.code === 'ArrowUp') && !isJumping) {
+            jump();
+        }
+    }); /*(if)Verifica si la tecla presionada es Space o ArrowUp y si no se está ejecutando ya un salto (!isJumping), entonces llama a la función jump().*/
+
+    function jump() {
+        isJumping = true; //Establece isJumping a true para evitar múltiples saltos simultáneos.
+        let jumpHeight = 100; // Altura del salto en píxeles
+        let jumpDuration = 300; // Duración del salto en milisegundos
+
+        note.style.bottom = `${jumpHeight}px`; //Anima el cambio de posición del note al establecer note.style.bottom a la altura del salto.
+
+        setTimeout(() => {
+            note.style.bottom = '0';
+            setTimeout(() => {
+                isJumping = false;
+            }, jumpDuration);
+        }, jumpDuration);
+    }
+}); // luego de un tiempo (jumpDuration), devuelve el note a su posición original y restablece isJumping a false para permitir otro salto.
+
+// END JS - WIP
