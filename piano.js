b62e3bef639ce4fc7f1d0fcb3e1c9a868a01fa64
juanvprada <juanvprada@gmail.com>
2024-07-22 13:48:06 +0200

    style:changing color and font

--- piano.js
+++ piano.js
@@ -1,4 +1,4 @@
-// Mostrar el modal cuando se carga la página
+// FUNCIÓN PARA MOSTRAR EL MODAL CON TIP CUANDO SE CARGAR LA PÁGINA
 window.onload = function () {
   let modal = document.getElementById("myModal");
   let span = document.getElementsByClassName("modal__close")[0];
@@ -10,7 +10,7 @@ window.onload = function () {
   }
 
 }
-// Contador de pulsaciones de teclas y MODAL
+// CONTADOR DE PULSACIONES DE TECLAS y MODAL PARA OFRECER PLANES
 let keyPressCount = 0;
 document.addEventListener('keypress', function () {
   keyPressCount++;
@@ -33,28 +33,27 @@ for (let i = 0; i < spans.length; i++) {
 
 
 // FUNCIÓN PARA ELEGIR QUE PARTITURA MOSTRAR  //
-function showSheetMusic(sheetmusic) {
-  let imagen = document.getElementById("sheetMusic");
-  if (sheetmusic.target.value === "2") {
-    imagen.setAttribute("src", "../public/assets/images/partituras/cumpleaños feliz.PNG")
+function showSheetMusic(sheetmusic) {   // Cuando el usuario selecciona un elemento del select el evento onchange del HTML activa esta función
+  let image = document.getElementById("sheetMusic");  //Obtenemos el elemento del HTML que tiene el id sheetMusic y se almacena en image
+  if (sheetmusic.target.value === "2") {    //Aquí decimos si la opción seleccionada en el select es la 2
+    image.setAttribute("src", "../public/assets/images/partituras/cumpleaños feliz.PNG")  //La imagen a mostrar va a ser la de la ruta especificada entre paréntesis, "src" hace referencia al atributo que queremos que cambie, "src" es la dirección d ela imágen a mostrar
   }
   if (sheetmusic.target.value === "3") {
-    imagen.setAttribute("src", "../public/assets/images/partituras/parti1.PNG")
+    image.setAttribute("src", "../public/assets/images/partituras/parti1.PNG")
   }
   if (sheetmusic.target.value === "4") {
-    imagen.setAttribute("src", "../public/assets/images/partituras/parti2.PNG")
+    image.setAttribute("src", "../public/assets/images/partituras/parti2.PNG")
   }
   if (sheetmusic.target.value === "5") {
-    imagen.setAttribute("src", "../public/assets/images/partituras/parti3.PNG")
+    image.setAttribute("src", "../public/assets/images/partituras/parti3.PNG")
   }
   if (sheetmusic.target.value === "6") {
-    imagen.setAttribute("src", "../public/assets/images/partituras/parti4.PNG")
+    image.setAttribute("src", "../public/assets/images/partituras/parti4.PNG")
   }
   if (sheetmusic.target.value === "1") {
-    imagen.setAttribute("src", "../public/assets/images/partituras/noche de paz.PNG")
+    image.setAttribute("src", "../public/assets/images/partituras/noche de paz.PNG")
   }
 }
-
 
 
 //FUNCIÓN PARA SILENCIAR LAS NOTAS //
@@ -73,10 +72,11 @@ function StopNotas() {
   sound5.pause();
   sound6.pause();
   sound7.pause();
+  
 }
 
 
-// Arrays de teclas y notas
+// ARRAYS QUE CONTIENEN TECLAS Y NOTAS
 const keys = ['d', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
 const notes = ['../public/assets/sounds/piano/do1.mp3', '../public/assets/sounds/piano/re1.mp3', '../public/assets/sounds/piano/mi1.mp3', '../public/assets/sounds/piano/fa1.mp3', '../public/assets/sounds/piano/sol1.mp3', '../public/assets/sounds/piano/la1.mp3', '../public/assets/sounds/piano/si1.mp3', '../public/assets/sounds/piano/do1.mp3', '../public/assets/sounds/piano/re1.mp3', '../public/assets/sounds/piano/mi1.mp3', '../public/assets/sounds/piano/fa1.mp3', '../public/assets/sounds/piano/sol1.mp3', '../public/assets/sounds/piano/la1.mp3', '../public/assets/sounds/piano/si1.mp3'];
 const drumNotes = ['../public/assets/sounds/bateria/Batdo.mp3', '../public/assets/sounds/bateria/Batre.mp3', '../public/assets/sounds/bateria/Batmi.mp3', '../public/assets/sounds/bateria/Batfa.mp3', '../public/assets/sounds/bateria/Batsol.mp3', '../public/assets/sounds/bateria/Batla.mp3', '../public/assets/sounds/bateria/Batsi.mp3','../public/assets/sounds/bateria/Batplado.mp3', '../public/assets/sounds/bateria/Batplare.mp3', '../public/assets/sounds/bateria/Batplami.mp3', '../public/assets/sounds/bateria/Batplafa.mp3', '../public/assets/sounds/bateria/Batplasol.mp3', '../public/assets/sounds/bateria/Batplala.mp3', '../public/assets/sounds/bateria/Batplasi.mp3'];
@@ -84,19 +84,18 @@ const guitarNotes = ['../public/assets/sounds/guitarra/Guido.mp3','../public/ass
 const fluteNotes = ['../public/assets/sounds/panflute/Fludo.mp3', '../public/assets/sounds/panflute/Flure.mp3', '../public/assets/sounds/panflute/Flumi.mp3', '../public/assets/sounds/panflute/Flufa.mp3', '../public/assets/sounds/panflute/Flusol.mp3', '../public/assets/sounds/panflute/Flula.mp3', '../public/assets/sounds/panflute/Flusi.mp3'];
 
 
-// Función para reproducir la nota
+// FUNCIÓN PARA REPRODUCIR LA NOTA
 function reproduceNote(note) {
   let audio = new Audio(note);
   audio.play();
 }
 
 
-// FUNCIÓN PARA ELEGIR EL INSTRUMENTO A SONAR //
-
+// FUNCIÓN PARA ELEGIR EL INSTRUMENTO A SONAR CON CONTROL DE FLUJO SWITCH//
 let currentNotes = notes;
-document.getElementById('miSelectSound').addEventListener('change', function (event) {
-  const miSelectSound = event.target.value;
-  switch (miSelectSound) {
+document.getElementById('miSelectSound').addEventListener('change', function (changing) {  //Selecciona el elemento HTML que tiene el id mySelectSound(es nuestro select) - luego decimos al elemento(mySelectSound) que se le aplique el evento 'change' que funciona cada vez que cambia el valor del select - y luego tenemos la función changing que se ejecuta cuando ocurre el evento 'change'
+  const mySelectedSound = changing.target.value;  //Creamos la variable myselectedSound que almacena el valor de la opción que se selecciona de entre los values del select del HTML-changing=función,target=mySelectSound y value=el valor de la opción seleccionada.
+  switch (mySelectedSound) {   //Con el control de flujo switch vamos diciendo en caso de que el valor seleccionado sea piano, vete al array notes, si el valor seleccionado es drums vete al array drumNotes y así....y si no por defecto vete al array notes
     case 'piano':
       currentNotes = notes;
       break;
@@ -125,7 +124,7 @@ document.addEventListener('click', function (clickMouse) {
 
     if (keys.includes(key)) { // Verifica si la tecla está en el array 'keys'
       let index = keys.indexOf(key); // Obtiene el índice de la tecla
-      let note = currentNotes[index]; // Obtiene la note correspondiente
+      let note = currentNotes[index]; // Obtiene la note de la posición correspondiente
       reproduceNote(note); // Reproduce la note
 
     }
@@ -133,18 +132,18 @@ document.addEventListener('click', function (clickMouse) {
 });
 
 
-// FUNCIÓN PARA QUE SUENEN LAS NOTAS AL PRESIONAR LAS TECLAS Y SE PONGA GRIS LA QUE SE TOQUE
+// FUNCIÓN PARA QUE SUENEN LAS NOTAS AL PRESIONAR LAS TECLAS Y SE PONGA GRIS LA TECLA QUE SE TOQUE
 document.addEventListener('keydown', function (preesKey) {
   let key = preesKey.key; // La tecla presionada
 
   if (keys.includes(key)) { // Verifica si la tecla está en el array 'keys'
     let index = keys.indexOf(key); // Obtiene el índice de la tecla
-    let note = currentNotes[index]; // Obtiene la note correspondiente
+    let note = currentNotes[index]; // Obtiene la note de la posición correspondiente
     reproduceNote(note); // Reproduce la note
 
-    let teclaElement = document.getElementById('key-' + key);
-    if (teclaElement) {
-      teclaElement.classList.add('key-gray');
+    let keyElement = document.getElementById('key-' + key); // Traemos el elemento del HTML que tenga el ID de key-letra y lo almacenamos en keyElement
+    if (keyElement) {  //Aquí verificamos que keyElement existe y si keyElement existe... (por ejemplo key-d que es el id de la tecla d)
+      keyElement.classList.add('key-gray'); //Decimos a keyElement aplícale la clase key-gray que tenemos creada en stylesPiano.css
     }
   }
 
@@ -162,9 +161,9 @@ document.addEventListener('keyup', function (keyUp) {
     let note = currentNotes[index]; // Obtiene la note correspondiente
     StopNotas(note); // silencia la nota
 
-    let teclaElement = document.getElementById('key-' + key);
-    if (teclaElement) {
-      teclaElement.classList.remove('key-gray');
+    let keyElement = document.getElementById('key-' + key);  // Traemos el elemento del HTML que tenga el ID de key-letra y lo almacenamos en keyElement
+    if (keyElement) {   //Aquí verificamos que keyElement existe y si keyElement existe... (por ejemplo key-d que es el id de la tecla d)
+      keyElement.classList.remove('key-gray');   //Decimos a keyElement elimina la clase key-gray que tenemos creada en stylesPiano.css
     }
   }
 
