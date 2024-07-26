0568b25b4ce5bc89e97043f9a04443ef0dd94ee3
juanvprada <juanvprada@gmail.com>
2024-07-26 14:19:27 +0200

    chore:css file link changed

--- src/scripts/piano.js
+++ src/scripts/piano.js
@@ -181,5 +181,16 @@ document.addEventListener('keyup', function (keyUp) {
 
 });
 
+//NAV BAR JS
+const nav = document.querySelector("#nav");
+const Abrir = document.querySelector("#Abrir");
+const Cerrar = document.querySelector("#Cerrar");
 
+Abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+Cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
 
