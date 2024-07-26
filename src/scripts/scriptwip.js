e239ee01ec8b3bf1160d8d4792049371d916f2cc
Monica <monicut3@gmail.com>
2024-07-26 14:34:13 +0200

    Add js function

--- src/scripts/scriptwip.js
+++ src/scripts/scriptwip.js
@@ -1,13 +1,14 @@
 // BEGIN JS - WIP
 
 //Selecciona el elemento con el id note, que es la figura dentro del juego donde se realizará la acción de salto.
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { //no espera a que cargue toda la página
     const note = document.getElementById('note');
     let isJumping = false; // Variable controla si el personaje está en medio de un salto para evitar saltos múltiples simultáneos.
 
 //Escucha el evento keydown para detectar cuando se presiona la tecla Space o ArrowUp
 document.addEventListener('keydown', event => {
     if ((event.code === 'Space' || event.code === 'ArrowUp') && !isJumping) {
+        event.preventDefault(); // Evita el comportamiento predeterminado (desplazamiento de la página)
         jump();
     }
 }); /*(if)Verifica si la tecla presionada es Space o ArrowUp y si no se está ejecutando ya un salto (!isJumping), entonces llama a la función jump().*/
