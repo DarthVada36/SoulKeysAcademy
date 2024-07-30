48b727085f10a39b0d73da42cca768be1a1edb8d
DarthVada36 <velazquez.vada@outlook.com>
2024-07-30 22:06:05 +0200

    Fixed: JS errors

--- src/scripts/scores.js
+++ src/scripts/scores.js
@@ -1,22 +1,33 @@
-let previewContainer = document.querySelector('scores-preview');
-let previewBox = previewContainer.querySelectorAll('preview-s');
+//contact.js
+import { burgerMenu, typingEffectVariousLines } from './main.js';
 
-document.querySelectorAll('scores-container .score').forEach(product =>{
-product.onclick = () =>{
-    previewContainer.computedStyleMap.display = 'flex';
-let name = product.getAttribute('data-name');
-previewBox.forEach(preview =>{
-    let target = preview.getAttribute('data-target');
-    if(name == target){
-        preview.classList.add('active');
-    }
-});
-};
+//Burger menu
+burgerMenu();
+
+//Typing effect
+const typing = document.querySelectorAll('.titleInstruments');
+typingEffectVariousLines(typing);
+
+//SCORES JS
+let previewContainer = document.querySelector('.scores-preview');
+let previewBox = previewContainer.querySelectorAll('.preview-s');
+
+document.querySelectorAll('.scores-container .score').forEach(product => {
+    product.onclick = () => {
+        previewContainer.style.display = 'flex';
+        let name = product.getAttribute('data-name');
+        previewBox.forEach(preview => {
+            let target = preview.getAttribute('data-target');
+            if (name == target) {
+                preview.classList.add('active');
+            }
+        });
+    };
 });
 
-previewBox.forEach(close =>{
-close.querySelector('ri-close-fill').onclick = () =>{
-    close.classList.remove('active');
-    previewContainer.computedStyleMap.display = 'none';
-};
+previewBox.forEach(close => {
+    close.querySelector('.ri-close-fill').onclick = () => {
+        close.classList.remove('active');
+        previewContainer.style.display = 'none';
+    };
 });
