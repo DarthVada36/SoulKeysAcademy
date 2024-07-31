2237e4680e5676228673a7d0929cad23844317ee
Monica <monicut3@gmail.com>
2024-07-31 10:59:51 +0200

    Feat: change css and js

--- src/scripts/scriptwip.js
+++ src/scripts/scriptwip.js
@@ -4,33 +4,54 @@ import {burgerMenu} from './main.js';
 // Burger menu
 burgerMenu();
 
-//Selecciona el elemento con el id note, que es la figura dentro del juego donde se realizará la acción de salto.
-document.addEventListener('DOMContentLoaded', () => { //no espera a que cargue toda la página
-    const note = document.getElementById('note');
-    let isJumping = false; // Variable controla si el personaje está en medio de un salto para evitar saltos múltiples simultáneos.
-
-//Escucha el evento keydown para detectar cuando se presiona la tecla Space o ArrowUp
-document.addEventListener('keydown', event => {
-    if ((event.code === 'Space' || event.code === 'ArrowUp') && !isJumping) {
-        event.preventDefault(); // Evita el comportamiento predeterminado (desplazamiento de la página)
-        jump();
-    }
-}); /*(if)Verifica si la tecla presionada es Space o ArrowUp y si no se está ejecutando ya un salto (!isJumping), entonces llama a la función jump().*/
-
-function jump() {
-    isJumping = true; //Establece isJumping a true para evitar múltiples saltos simultáneos.
-    let jumpHeight = 100; // Altura del salto en píxeles
-    let jumpDuration = 300; // Duración del salto en milisegundos
-
-    note.style.bottom = `${jumpHeight}px`; //Anima el cambio de posición del note al establecer note.style.bottom a la altura del salto.
-
-    setTimeout(() => {
-        note.style.bottom = '0';
-        setTimeout(() => {
-            isJumping = false;
-        }, jumpDuration);
-    }, jumpDuration);
+// Función para obtener el valor del parámetro en la URL
+function getParametroURL(parametro) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(parametro);
 }
-}); // luego de un tiempo (jumpDuration), devuelve el note a su posición original y restablece isJumping a false para permitir otro salto.
 
-// END JS - WIP
+// Obtener el instrumento seleccionado
+const instrument = getParametroURL('instrument');
+
+// Cambiar el fondo según el instrumento
+switch (instrument) {
+    case 'guitar':
+        document.body.style.backgroundImage = "url('../public/assets/images/guitar/instrument-icon.png')";
+        break;
+    case 'drums':
+        document.body.style.backgroundImage = "url('../public/assets/images/drum-set-icon.png')";
+        break;
+    case 'tuba':
+        document.body.style.backgroundImage = "url('../public/assets/images/tuba-icon.png')";
+        break;
+
+    /*default:
+            document.body.style.backgroundImage = "none";
+            break;*/
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const note = document.getElementById('note');
+    let isJumping = false;
+
+    document.addEventListener('keydown', event => {
+        if ((event.code === 'Space' || event.code === 'ArrowUp') && !isJumping) {
+            jump();
+        }
+    });
+
+    function jump() {
+        isJumping = true;
+        let jumpHeight = 100; // Altura del salto en píxeles
+        let jumpDuration = 300; // Duración del salto en milisegundos
+
+        note.style.bottom = `${jumpHeight}px`;
+
+        setTimeout(() => {
+            note.style.bottom = '0';
+            setTimeout(() => {
+                isJumping = false;
+            }, jumpDuration);
+        }, jumpDuration);
+    }
+});
