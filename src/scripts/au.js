2c007562aa6eaf4849fee41fa8142b206a569c60
LorelizDev <loreliz.dev@gmail.com>
2024-08-01 11:35:12 +0200

    feat: add typing effect

---
+++ src/scripts/au.js
@@ -0,0 +1,8 @@
+import { burgerMenu, typingEffectVariousLines } from './main.js';
+
+// Burger menu
+burgerMenu();
+
+// Typing effect
+const typing = document.querySelectorAll('.about-us__title--highlight');
+typingEffectVariousLines(typing);
